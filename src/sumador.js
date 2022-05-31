d21723c408b8cbf7aa02e6b2770b445602631ec2
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-05-31 11:41:07 -0400

    monto 10 y efectivo 15devuelve 5

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,5 @@
-function sumar(a, b) {
-  return a + b;
+function cambio(a, b) {
+  return a - b;
 }
 
-export default sumar;
+export default cambio;
--- src/ventas.Ul.test.js
+++ src/ventas.Ul.test.js
@@ -11,4 +11,10 @@ describe("Cambio de venta", () => {
     cambio.click();
     expect(monto.innerHTML).toEqual("0");
   });
+  it("Si el monto ingresado es 10 y el efecftivo 15 debe ser devuelto 5 ", () => {
+    const monto = document.querySelector("#monto");
+    const cambio = document.querySelector("#botonCambio");
+    cambio.click();
+    expect(div.innerHTML).toEqual("5");
+  });
 });
