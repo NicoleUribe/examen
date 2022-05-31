e7173fe33957b7e34e01fb704f4086177e89ebe2
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-05-31 11:58:41 -0400

    1.Recibir un monto y devolverlo-listo

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -3,6 +3,6 @@ import sumar from "./sumador.js";
 const frac = new sumar();
 describe("Fraccionar", () => {
   it("1.Recibir un monto y devolverlo", () => {
-    expect(fraccionar(5)).toEqual(5);
+    expect(frac.fraccionar(5)).toEqual(5);
   });
 });
