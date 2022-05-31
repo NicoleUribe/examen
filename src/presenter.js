c8050d95a983a8d0f25574e8779028ba5f530a5a
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-05-31 11:39:20 -0400

    devuelve 0

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,15 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const Monto = document.querySelector("#venta");
+const efectivo = document.querySelector("#montEfectivo");
+const form = document.querySelector("#cambio-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(Monto.value);
+  const secondNumber = Number.parseInt(efectivo.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + cambio(firstNumber, secondNumber) + "</p>";
 });
